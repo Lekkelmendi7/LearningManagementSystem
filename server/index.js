e3051cea5a3c69b1021e7faf54dd310f80adee04
Lekkelmendi7 <lek.kelmendi8.com>
2024-07-22 18:42:51 +0200

    Making routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import {connectDb} from './database/db.js';
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDb } from './database/db.js';
+import userRoutes from '../server/routes/user.js'; // Ensure this path is correct
 
 dotenv.config();
 
 const app = express();
-
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
     res.send("Server is working");
 });
 
+// Using routes
+app.use("/api", userRoutes);
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`); 
+    console.log(`Server is running on port http://localhost:${port}`);
     connectDb();
 });
