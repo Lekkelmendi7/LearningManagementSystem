e79452a5a0c2a5ac2c0faf4fb8c36c566ff45ff0
Lekkelmendi7 <lek.kelmendi8.com>
2024-07-24 22:19:05 +0200

    Fetching lectures of a course

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -19,3 +19,18 @@ export const getSingleCourse = TryCatch(async (req, res) => {
     });
 });
 
+export const fetchLectures = TryCatch(async (req, res) => {
+    const lectures = await Lecture.find({course: req.params.id})
+
+    const user = await User.findById(req.user._id)
+
+    if(user.role === "admin") {
+        return res.json({lectures});
+    }
+
+    if(!user.subscription.includes(req.params.id))
+        return res.status(400).json({
+            message: "You didn't subscribe to this course"
+        });
+    res.json({lectures});
+});
