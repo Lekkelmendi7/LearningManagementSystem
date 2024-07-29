154c72ef4edc822b165bc87ab341667eef4b4774
Lekkelmendi7 <95182111+Lekkelmendi7@users.noreply.github.com>
2024-07-29 15:56:43 +0200

    Authentication pages added

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import App from './App.css';
+import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from '../pages/home/Home';
+import Home from './pages/home/Home';
+import Header from './components/header/Header';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Verify from './pages/auth/Verify';
 
 const App = () => {
   return <>
@@ -9,9 +13,12 @@ const App = () => {
   <Header />
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path='/login' element={<Login/>} />
+    <Route path='/register' element={<Register />} />
+    <Route path='/verify' element={<Verify />} />
   </Routes>
   </BrowserRouter>
   </> 
 }
 
-export default App
+export default App;
--- frontend/vite-project/src/components/header/Header.jsx
+++ frontend/vite-project/src/components/header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import "./header.css";
-
+import {Link} from "react-router-dom";
 const Header = () => {
   return (
 <header>
     <div className='logo'> E-Learning </div>
     <div className='link'>
-        
+        <Link to={"/"}>Home</Link>
+        <Link to={"/courses"}>Courses</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/account"}>Account</Link>
     </div>
 </header>
     
@@ -14,4 +17,4 @@ const Header = () => {
   
 }
 
-export default Header
+export default Header;
