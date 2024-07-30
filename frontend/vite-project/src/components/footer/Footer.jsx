6a20335660f47f9be32f46df28387125f2844f3a
Lekkelmendi7 <95182111+Lekkelmendi7@users.noreply.github.com>
2024-07-30 10:09:50 +0200

    Creating a footer

---
+++ frontend/vite-project/src/components/footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./footer.css";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillLinkedin
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>
+          &copy; 2024 Your E-Learning App. All rights reserved. <br /> Created by <a href="https://www.linkedin.com/in/lek%C3%AB-kelmendi/">Leke Kelmendi</a>
+        </p>
+        <div className="social-links">
+          <a href="https://www.facebook.com/profile.php?id=61560795113653">
+            <AiFillFacebook />
+          </a>
+          <a href="https://www.linkedin.com/in/lek%C3%AB-kelmendi/">
+            <AiFillLinkedin />
+          </a>
+          <a href="https://www.instagram.com/lek.kelmendi/">
+            <AiFillInstagram />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
