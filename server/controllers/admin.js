ddb9e96ea910fb7e68354c9d5e9bb6149eec1188
Lekkelmendi7 <lek.kelmendi8.com>
2024-08-20 18:31:19 +0200

    Getting all the statistics: Number of total courses, usrs and lectures on the admin dashboard

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,70 +1,69 @@
-import TryCatch from '../middleware/TryCatch.js';
-import {Courses} from '../models/Courses.js';
+import TryCatch from "../middleware/TryCatch.js";
+import { Courses } from "../models/Courses.js";
 import { Lecture } from "../models/Lecture.js";
-import {rm} from 'fs';
+import { rm } from "fs";
 import { promisify } from "util";
 import fs from "fs";
 import { User } from "../models/User.js";
 
-
 export const createCourse = TryCatch(async (req, res) => {
-    const {title, description, category, createdBy, duration, price} = req.body;
+  const { title, description, category, createdBy, duration, price } = req.body;
 
-    const image = req.file;
+  const image = req.file;
 
-    await Courses.create({
-        title,
-        description,
-        category,
-        createdBy,
-        image: image?.path,
-        duration,
-        price,
-    });
+  await Courses.create({
+    title,
+    description,
+    category,
+    createdBy,
+    image: image?.path,
+    duration,
+    price,
+  });
 
-res.status(201).json({
+  res.status(201).json({
     message: "Course Created Successfully",
-    });
+  });
 });
 
 export const addLectures = TryCatch(async (req, res) => {
-    const course = await Courses.findById(req.params.id);
+  const course = await Courses.findById(req.params.id);
 
-    if(!course)
-        return res.status(404).json({
-    message: "No Course with this id",
+  if (!course)
+    return res.status(404).json({
+      message: "No Course with this id",
     });
 
-    const {title, description} = req.body
+  const { title, description } = req.body;
 
-    const file = req.file
+  const file = req.file;
 
-    const lecture = await Lecture.create({
-        title,
-        description,
-        video: file?.path,
-        course: course._id,
-    });
+  const lecture = await Lecture.create({
+    title,
+    description,
+    video: file?.path,
+    course: course._id,
+  });
 
-    res.status(201).json({
-        message: "Lecture added!",
-        lecture,
-    });
+  res.status(201).json({
+    message: "Lecture Added",
+    lecture,
+  });
 });
 
 export const deleteLecture = TryCatch(async (req, res) => {
-    const lecture = await Lecture.findById(req.params.id);
-  
-    rm(lecture.video, () => {
-      console.log("Video deleted");
-    });
-  
-    await lecture.deleteOne();
-  
-    res.json({ message: "Lecture Deleted" });
+  const lecture = await Lecture.findById(req.params.id);
+
+  rm(lecture.video, () => {
+    console.log("Video deleted");
   });
 
-  const unlinkAsync = promisify(fs.unlink);
+  await lecture.deleteOne();
+
+  res.json({ message: "Lecture Deleted" });
+});
+
+const unlinkAsync = promisify(fs.unlink);
 
 export const deleteCourse = TryCatch(async (req, res) => {
   const course = await Courses.findById(req.params.id);
@@ -94,19 +93,51 @@ export const deleteCourse = TryCatch(async (req, res) => {
 });
 
 export const getAllStats = TryCatch(async (req, res) => {
-  const totalCourses = (await Courses.find()).length;
-  const totalLecturtes = (await Lecture.find()).length;
+  const totalCoures = (await Courses.find()).length;
+  const totalLectures = (await Lecture.find()).length;
   const totalUsers = (await User.find()).length;
 
   const stats = {
-    totalCourses,
-    totalLecturtes,
-    totalUsers
+    totalCoures,
+    totalLectures,
+    totalUsers,
   };
 
   res.json({
     stats,
   });
-  
+});
 
-})
+export const getAllUser = TryCatch(async (req, res) => {
+  const users = await User.find({ _id: { $ne: req.user._id } }).select(
+    "-password"
+  );
+
+  res.json({ users });
+});
+
+export const updateRole = TryCatch(async (req, res) => {
+  if (req.user.mainrole !== "superadmin")
+    return res.status(403).json({
+      message: "This endpoint is assign to superadmin",
+    });
+  const user = await User.findById(req.params.id);
+
+  if (user.role === "user") {
+    user.role = "admin";
+    await user.save();
+
+    return res.status(200).json({
+      message: "Role updated to admin",
+    });
+  }
+
+  if (user.role === "admin") {
+    user.role = "user";
+    await user.save();
+
+    return res.status(200).json({
+      message: "Role updated",
+    });
+  }
+});
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,14 +1,24 @@
-import express from 'express';
-import { addLectures, createCourse, deleteLecture, deleteCourse, getAllStats, } from '../controllers/admin.js';
-import { isAuth, isAdmin } from '../middleware/isAuth.js';
-import { uploadFiles } from '../middleware/multer.js';
+import express from "express";
+import { isAdmin, isAuth } from "../middleware/isAuth.js";
+import {
+  addLectures,
+  createCourse,
+  deleteCourse,
+  deleteLecture,
+  getAllStats,
+  getAllUser,
+  updateRole,
+} from "../controllers/admin.js";
+import { uploadFiles } from "../middleware/multer.js";
 
 const router = express.Router();
 
-router.post('/course/new', isAuth, isAdmin, uploadFiles, createCourse);
-router.post('/course/:id', isAuth, isAdmin, uploadFiles, addLectures);
-router.delete("/lectures/:id", isAuth, isAdmin, deleteLecture);
+router.post("/course/new", isAuth, isAdmin, uploadFiles, createCourse);
+router.post("/course/:id", isAuth, isAdmin, uploadFiles, addLectures);
 router.delete("/course/:id", isAuth, isAdmin, deleteCourse);
+router.delete("/lecture/:id", isAuth, isAdmin, deleteLecture);
 router.get("/stats", isAuth, isAdmin, getAllStats);
+router.put("/user/:id", isAuth, updateRole);
+router.get("/users", isAuth, isAdmin, getAllUser);
 
 export default router;
