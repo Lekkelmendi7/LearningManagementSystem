971207a0c5f9f81b527503521976ad35df42fe15
Lekkelmendi7 <lek.kelmendi8.com>
2024-08-24 13:57:48 +0200

    Adding the progress bar for course lectures

--- server/models/User.js
+++ server/models/User.js
@@ -1,39 +1,39 @@
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
 
 const schema = new mongoose.Schema(
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-      role: {
-        type: String,
-        default: "user",
-      },
-      mainrole: {
-        type: String,
-        default: "user",
-      },
-      subscription: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Courses",
-        },
-      ],
-      resetPasswordExpire: Date,
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-      timestamps: true,
-    }
-  );
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    mainrole: {
+      type: String,
+      default: "user",
+    },
+    subscription: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Courses",
+      },
+    ],
+    resetPasswordExpire: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
 export const User = mongoose.model("User", schema);
--- server/routes/user.js
+++ server/routes/user.js
@@ -8,14 +8,17 @@ import {
   verifyUser,
 } from "../controllers/user.js";
 import { isAuth } from "../middlewares/isAuth.js";
+import { addProgress, getYourProgress } from "../controllers/course.js";
 
- const router = express.Router();
+const router = express.Router();
 
- router.post("/user/register", register);
- router.post("/user/verify", verifyUser);
- router.post("/user/login", loginUser);
- router.get("/user/me", isAuth, myProfile);
- router.post("/user/forgot", forgotPassword);
- router.post("/user/reset", resetPassword);
+router.post("/user/register", register);
+router.post("/user/verify", verifyUser);
+router.post("/user/login", loginUser);
+router.get("/user/me", isAuth, myProfile);
+router.post("/user/forgot", forgotPassword);
+router.post("/user/reset", resetPassword);
+router.post("/user/progress", isAuth, addProgress);
+router.get("/user/progress", isAuth, getYourProgress);
 
- export default router;
+export default router;
